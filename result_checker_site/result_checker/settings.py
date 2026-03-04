8aed18cc7427240239bcce70458821aa938d920c
SuprimOjha <suprimojha25@gmail.com>
2026-03-04 21:22:51 +0545

    error fixed

--- result_checker_site/result_checker/settings.py
+++ result_checker_site/result_checker/settings.py
@@ -148,10 +148,10 @@ LOGIN_REDIRECT_URL = 'admin_dashboard'
 
 from decouple import config
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+EMAIL_HOST_USER = "suprimojha25@gmail.com"
+EMAIL_HOST_PASSWORD = "xmge kttj armn gxjp"
